d8f4b78dd99e4537ae8fd7af51e2bfc0da5a1ff0
danielbutts <daniel.butts@gmail.com>
2017-05-22 11:25:11 -0700

    complted warmup

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('Your code goes here!')
+$('form').submit(submitForm);
+$(document).keyup(checkForm);
+$('#category').change(checkForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  let task = $('#task').val();
+  let category = $('#category').val();
+  addTask(task, category);
+  setTaskCount($('ul').children().length);
+  $('#task').val('');
+  $('#category').val('');
+}
+
+function addTask(task, category) {
+  $('ul').append(`<li>${task}<span class="badge alert-success">${category}</span>
+    <a class="delete-button"><span class="remove-me badge alert-danger">Remove Me</span></a></li>`);
+  $(`#tasks`).on('click', '.delete-button', deleteTask);
+}
+
+function checkForm() {
+  console.log('val',$('#category').val(),$('#task').val());
+  if ($('#task').val() !== '' && $('#category').val() !== '') {
+    console.log('yay');
+    $('button').prop("disabled", false);
+  } else {
+    console.log('nope');
+    $('button').prop("disabled", true);
+  }
+}
+
+function deleteTask(e) {
+  e.preventDefault();
+  let item = e.target.closest('li');
+  item.remove();
+  setTaskCount($('ul').children().length);
+}
+
+function setTaskCount(num) {
+  $('#taskCount').empty();
+  $('#taskCount').append(num);
+}
